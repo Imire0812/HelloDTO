c123fceeab0b9723c9e565d0c41de319aad50fb0
iman imire <33391276+Imire0812@users.noreply.github.com>
2023-09-27 20:21:42 +0200

    feat class

---
+++ src/main/java/hellodto/HelloDtoApplication.java
@@ -0,0 +1,13 @@
+package hellodto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hellodto/HelloDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package hellodto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
